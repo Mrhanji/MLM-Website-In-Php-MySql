92c2ccd2f468ef4302619aea53d2dbf3c71d84a1
Sagar Sharma <sagarsharma995899@gmail.com>
2021-10-21 19:32:56 +0530

    v1(beta)

--- auth/user/config.php
+++ auth/user/config.php
@@ -1,20 +1,12 @@
 <?php
 
 $servername = "localhost";
-$username = "u260543756_bichoobag";
-$password = "0L=5:?hE";
-$db ="u260543756_bichoobag";
+$username = "";
+$password = "";
+$db ="";
 
 $connection = mysqli_connect($servername, $username, $password, $db);
 
 
-
-//$connection= mysqli_connect("bichoobag.in", "bichooba_mlmuser" , "EZD.Px$}+F#v" ,"bichooba_db") or die("Connection Failed :". mysqli_connect_error());
-
-
-
-//$connection= mysqli_connect("bichoobag.in", "bichooba_mlm" , "9MpC&o&mdR@G" ,"bichooba_test") or die("Connection Failed :". mysqli_connect_error());
-
-//$siteurlref ="http://localhost/bichoobag/user/";
 
 ?>
