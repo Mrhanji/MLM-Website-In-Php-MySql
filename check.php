2648f8351a3791afe206877be05ee3bacf8208a4
Sagar Sharma <sagarsharma995899@gmail.com>
2021-10-21 19:23:51 +0530

    first commit

---
+++ check.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 11px 10%;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.button2 {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #008CBA;
+}
+
+.button2:hover {
+  background-color: #008CBA;
+  color: white;
+  
+}
+
+
+}
+footer{
+height:551px;
+width:55px;
+background-color :black;
+}
+</style>
+</head>
+<body>
+<center><h1><b>Your Account was Approved By Admin Enjoy..</b></h1><br><br>
+<p>Click here to login</p>
+<a href="https://bichoobag.in/" class="button button2">Login</a>
+
+<br>
+<?php
+echo "dddd";?>
+
+
+</body>
+</html>
